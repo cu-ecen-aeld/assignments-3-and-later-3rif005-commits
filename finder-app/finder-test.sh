abf072bb8bef3d594d0049e52ef72b225d387077
3rif005 <3rif005@gmail.com>
2026-01-21 10:34:00 +0100

    Re-apply Assignment 4 finder-test.sh fixes

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Tester script for assignment 1 and assignment 2
+# Tester script for assignment 4
 # Author: Siddhant Jajoo
 
 set -e
@@ -8,21 +8,29 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# Requirement: Config files are at /etc/finder-app/conf
+CONF_DIR=/etc/finder-app/conf
+
+# If the config directory doesn't exist, fall back to local conf (for local testing)
+if [ ! -d "${CONF_DIR}" ]; then
+    CONF_DIR=conf
+fi
+
+username=$(cat ${CONF_DIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi      
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -31,33 +39,30 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=$(cat conf/assignment.txt)
+# Requirement: Config files are at /etc/finder-app/conf
+assignment=$(cat ${CONF_DIR}/assignment.txt)
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+    mkdir -p "$WRITEDIR"
+    if [ -d "$WRITEDIR" ]; then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    # Requirement: Run with executables found in the PATH
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# Requirement: Run with executables found in the PATH
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+# Requirement: Write output to /tmp/assignment4-result.txt
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
@@ -65,9 +70,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
